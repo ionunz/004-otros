6e251e4d862353ccf7cf7071d3ea2c6a5366410a
Jonathan Núñez <estudionunz@gmail.com>
2019-10-24 13:18:32 -0500

    Agrego ejercicio práctico conjunto de HTML / CSS / JS

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 $('button').on('click', function(){
-    $(this).siblings('p').show()
+    $('p').hide();
+    $(this).siblings('p').show();
+})
+
+const fechaLanzamiento = new Date('10/22/19 18:55');
+let fechaActual = new Date();
+
+if(fechaActual > fechaLanzamiento){
+    $('.video').removeClass('hidden');
+    $('.comingsoon').addClass('hidden');
+}
+
+let Alf = true;
+
+$('.toggle button').on('click', function(){
+    if(Alf === true){
+        $('.toggle img').removeClass('hidden');
+        Alf = false;
+    }
+    else{
+        $('.toggle img').addClass('hidden');
+        Alf = true;
+    }
 })
